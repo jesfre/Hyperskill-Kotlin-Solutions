d653de6b930c3cc0a122cf8f363cd129fc82156e
jesfre <jorge.ruiz.aquino@gmail.com>
2019-03-28 11:51:22 -0400

    Completed "Data types"

---
+++ src/com/blogspot/jesfre/kotlin/datatypesandvariables/datatypes/FixProgram.kt
@@ -0,0 +1,6 @@
+package com.blogspot.jesfre.kotlin.datatypesandvariables.datatypes
+
+fun main(args: Array<String>) {
+    val number: Int = 100
+    println(number)
+}
---
+++ src/com/blogspot/jesfre/kotlin/datatypesandvariables/datatypes/FixProgram2.kt
@@ -0,0 +1,6 @@
+package com.blogspot.jesfre.kotlin.datatypesandvariables.datatypes
+
+fun main(args: Array<String>) {
+    val number = "Hello, Kotlin!"
+    println(number)
+}
