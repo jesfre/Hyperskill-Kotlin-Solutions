94effd5c2b56f8243cc5145ea2e3c959c52fa72a
jesfre <jorge.ruiz.aquino@gmail.com>
2019-03-28 11:53:09 -0400

    Completed "The classification of basic types"

---
+++ src/com/blogspot/jesfre/kotlin/datatypesandvariables/classification/MaxOfInteger.kt
@@ -0,0 +1,8 @@
+package com.blogspot.jesfre.kotlin.datatypesandvariables.classification
+
+fun main(args: Array<String>) {
+    println(Byte.MAX_VALUE)
+    println(Short.MAX_VALUE)
+    println(Int.MAX_VALUE)
+    println(Long.MAX_VALUE)
+}
---
+++ src/com/blogspot/jesfre/kotlin/datatypesandvariables/classification/SizeMinMax.kt
@@ -0,0 +1,7 @@
+package com.blogspot.jesfre.kotlin.datatypesandvariables.classification
+
+fun main(args: Array<String>) {
+    println(Int.SIZE_BITS)
+    println(Int.MIN_VALUE)
+    println(Int.MAX_VALUE)
+}
