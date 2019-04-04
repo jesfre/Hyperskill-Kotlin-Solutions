c0a9ca8494a83be308adc697f51ce7a18d8f448f
jesfre <jorge.ruiz.aquino@gmail.com>
2019-04-04 09:31:07 -0400

    Completed project Coffee Machine Stage 1

---
+++ src/com/blogspot/jesfre/kotlin/projects/coffeemachine/stage1/MakeCoffee.kt
@@ -0,0 +1,21 @@
+package com.blogspot.jesfre.kotlin.projects.coffeemachine.stage1
+
+/*
+Description
+Let's start with a program that makes you a coffee – virtual coffee, of course.
+But in this project, you will implement functionality that simulates a real coffee machine.
+It can run out of milk, it can run out of coffee beans, it can make different varieties of coffee,
+and it can take the money for making a coffee.
+
+The first version of the program just makes you a coffee.
+It prints to the standard output what it is doing as it makes a coffee.
+ */
+fun main() {
+    println("Starting to make a coffee")
+    println("Grinding coffee beans")
+    println("Boiling water")
+    println("Mixing boiled water with crushed coffee beans")
+    println("Pouring coffee into the cup")
+    println("Pouring some milk into the cup")
+    println("Coffee is ready!")
+}
